d4eab0b3ef2b07927ab818409a501160c84a4c3e
irpannawawi <irpannawawi.ixd@gmail.com>
2022-01-06 09:28:28 +0000

    init

---
+++ config.php
@@ -0,0 +1,11 @@
+<?php
+defined('BASEPATH') OR exit('No direct script access allowed');
+$host = "159.223.49.36";
+$databaseuser = "rdm";
+$databasename = "rdmdb";
+$dbpassword = "rdmPassword3";
+define("DB_SERVER",$host);
+define("DB_USERNAME",$databaseuser);
+define("DB_PASSWORD",$dbpassword);
+define("DB_DATABASE",$databasename);
+?>
